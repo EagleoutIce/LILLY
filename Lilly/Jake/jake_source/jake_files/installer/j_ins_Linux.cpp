6084118da7d7d8106c176556ef1fb7470d30d679
EagleoutIce <maeusepiep99@web.de>
2019-04-20 12:18:00 +0200

    Kleiner Fix bei Jake

--- Lilly/Jake/jake_source/jake_files/installer/j_ins_Linux.cpp
+++ Lilly/Jake/jake_source/jake_files/installer/j_ins_Linux.cpp
@@ -36,11 +36,13 @@ status_t ins_linux( void ) {
             std::cout << "Installiere 'texlive-full' via _apt_ : " << er_decode(system("sudo apt install texlive-full")) << std::endl;
         else
             std::cout << "  Jake installiert LILLY nun weiter, ohne 'pdflatex', da  du (n)o gewählt hast!" << std::endl;
-    }  
+    }
 
     std::cout << "  - Erstelle (" <<  settings["install-path"] << "/tex/latex): "
               << er_decode(system(("mkdir -p " + settings["install-path"] + "/tex/latex").c_str()))
               << std::endl;
+
+    c_inp='\0';
 
     // Expand path:
     if (!check_file(exec("echo -n " + settings[S_LILLY_PATH])+"/Lilly.cls")) {
@@ -71,7 +73,7 @@ status_t ins_linux( void ) {
         }
 
     } //URGENT TODO: AUSFÜHRLICHE SUChE
-    
+
     std::cout << "  - Verlinke (" << settings[S_LILLY_PATH] << " = " << exec("echo -n " + settings[S_LILLY_PATH]) << ") nach (" << settings["install-path"] << "/tex/latex): "
               << er_decode(system(("ln -sf "+settings[S_LILLY_PATH]+" "+settings["install-path"]+"/tex/latex").c_str()))
               << std::endl;
@@ -83,20 +85,20 @@ status_t ins_linux( void ) {
     std::cout << "  - Informiere TEX über (" << settings["install-path"] << "): "
               << er_decode(system(("texhash " + settings["install-path"] + " > /dev/null").c_str()))
               << std::endl;
-    
+
     std::cout << "  ? Experimental sucht Jake alle Pakete heraus die Lilly benötigen könnte um dann, " << std::endl << "    in einer späteren Version, Variable Installationen anzubieten :D" << std::endl;
-    
+
     //for i in $(cd ~/texmf/tex/latex/Lilly/ && grep -E "(LILLYxDemandPackage|LILLYxLoadPackage){([a-zA-Z]+[a-zA-Z0-9]*)}" -r * --exclude=_LILLY_PACKAGE_CTRL.tex -hos | grep -E "{[a-zA-Z]+[a-zA-Z0-9]*" -os); do if [[ "$i" == "LILLYxLoadPackage" ]]; then export a=load && continue; elif [[ "$i" == "LILLYxDemandPackage" ]]; then export a=demand && continue; fi;  echo "- ($a): $i"; done;
-    
+
     std::cout << "Sammlung der Pakete abgeschlossen: " << er_decode(system(("for i in $(cd " + settings["install-path"] + "/tex/latex && grep -E " + \
                     "\"(LILLYxDemandPackage|LILLYxLoadPackage){([a-zA-Z]+[a-zA-Z0-9]*)}\" -r * " + \
                     "-hos | grep -E '[a-zA-Z]+[a-zA-Z0-9]*' -os); do if " +\
                     "[ \"$i\" = \"LILLYxLoadPackage\" ]; then export a=load && continue; elif " + \
                     "[ \"$i\" = \"LILLYxDemandPackage\" ]; then export a=demand && continue; fi; " +\
                     "echo \"      - ($a): $i\"; done;").c_str())) << std::endl;
-    
-    
+
+
     std::cout << "Der Installationsprozess wurde abgeschlossen :D" << std::endl;
-    
+
     return EXIT_SUCCESS;
 }
