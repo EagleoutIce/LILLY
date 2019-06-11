06a6b1b9694678ae7a5f5a605b5302ef7c052ba9
EagleoutIce <maeusepiep99@web.de>
2019-06-11 21:44:33 +0200

    Additional Paper and Test fix!
    Paper-Layout kümmert sich nun automatisch um Bibtex, liefert \ABSTRACT für den Abstract text,
    und bietet \printLILLY für einen Disclaimer. Weiter wurden die notwendigen Pakete direkt eingebunden.
    Eine exemplarische Konfiguration für Jake findet sich hier:
    ```
    operation	=	file_compile
    file		=	@[SELTEXF]
    
    lilly-boxes =   LIMERENCE
    
    lilly-modes = default
    
    lilly-autoclean = false
    lilly-bibtex    = books.bib
    
    lilly-show-boxname  = false
    ```
    Information: in diesem Fall muss eine bibtex-Datei mit den Namen books.bib im selben Verzeichnis wie die .tex datei angegeben werden.
    
    Signed-off-by: EagleoutIce <maeusepiep99@web.de>

--- Jake/src/main/java/de/eagle/lillyjakeframework/core/CoreSettings.java
+++ Jake/src/main/java/de/eagle/lillyjakeframework/core/CoreSettings.java
@@ -17,23 +17,16 @@ import static de.eagle.util.logging.JakeLogger.writeLoggerInfo;
 
 public class CoreSettings {
 
-
     public static Settings settings = null;
 
-    private static SettingsTranslator translator;
-
-    static {
-        try {
-            translator = new SettingsTranslator("CoreTranslator","/translators/CoreSettings.trans");
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
+    private static SettingsTranslator translator = null;
 
     /**
      * @return die enthaltenen Einstellungen
      */
     public static Settings getSettings() {
+        if (settings == null)
+            init();
         return settings;
     }
 
@@ -42,35 +35,43 @@ public class CoreSettings {
      *
      * @return der verwendete Translator
      */
-    public static SettingsTranslator getTranslator(){
+    public static SettingsTranslator getTranslator() {
+        if (translator == null) {
+            try {
+                translator = new SettingsTranslator("CoreTranslator", "/translators/CoreSettings.trans");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
         return translator;
     }
 
     /**
      * Initialisiert die Einstellungen mit den entsprechendne Standarts
+     *
      * @return true, wenn eine die Einstellungen generiert werden mussten.
      */
     public static boolean init() {
-        if(settings != null) {
-            writeLoggerInfo("Es wurde unnötiger weise CoreSettings::Init aufgerufen!","CoreS");
+        if (settings != null) {
+            writeLoggerInfo("Es wurde unnötiger weise CoreSettings::Init aufgerufen!", "CoreS");
             // Dies könnte nicht gewollt sein
             return false;
         }
         writeLoggerInfo("Es werden neue Einstellungen für CoreSettings generiert (init)", "CoreS");
         try {
             SettingsTranslator st = new SettingsTranslator("CoreSettings");
-            settings = new Settings("<Jake> CoreSettings",true,new HashMap<>());
+            settings = new Settings("<Jake> CoreSettings", true, new HashMap<>());
 
-            settings.emplace(st,"S_VERSION","Jake-Version", eSetting_Type.IS_TEXT, JAKE_VERSION);
+            settings.emplace(st, "S_VERSION", "Jake-Version", eSetting_Type.IS_TEXT, JAKE_VERSION);
 
-            // Hier wurden schon einemal die für Buildrues wichtigen Einstellungen eingefügt:
+            // Hier wurden schon einemal die für Buildrues wichtigen Einstellungen
+            // eingefügt:
 
-            settings.emplace(st,"S_LILLY_OUT","Ausgabeordner der PDF-DATEI", eSetting_Type.IS_PATH, "./OUTPUT/");
+            settings.emplace(st, "S_LILLY_OUT", "Ausgabeordner der PDF-DATEI", eSetting_Type.IS_PATH, "./OUTPUT/");
 
-            settings.emplace(st,"S_LILLY_NAMEPREFIX","Standart-Namenspräfixs", eSetting_Type.IS_TEXT, "");
-            settings.emplace(st,"S_LILLY_COMPLETE_NAME","Präfix einer Complete-Version", eSetting_Type.IS_TEXT, "COMPLETE-");
-
-
+            settings.emplace(st, "S_LILLY_NAMEPREFIX", "Standart-Namenspräfixs", eSetting_Type.IS_TEXT, "");
+            settings.emplace(st, "S_LILLY_COMPLETE_NAME", "Präfix einer Complete-Version", eSetting_Type.IS_TEXT,
+                    "COMPLETE-");
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
@@ -84,64 +85,62 @@ public class CoreSettings {
     /**
      * Platziert eine neue Einstellung mithilfe eines Translators
      *
-     * @param key der entsprechende Schlüssel
+     * @param key   der entsprechende Schlüssel
      * @param brief Erklärung zu Einstellung
-     * @param type Der Typ der Einstellung
-     * @param init Der initiale wert
+     * @param type  Der Typ der Einstellung
+     * @param init  Der initiale wert
      *
      * @see SettingDeskriptor#create(String, String, eSetting_Type, Serializable)
      *
      * @return das alte Objekt, wenn vorhanden
      */
-    public SettingDeskriptor<String> emplace(String key, String brief, eSetting_Type type, String init){
+    public SettingDeskriptor<String> emplace(String key, String brief, eSetting_Type type, String init) {
         return settings.emplace(translator, key, brief, type, init);
     }
 
     /**
      * Platziert eine neue Einstellung mithilfe eines Translators
      *
-     * @param key der entsprechende Schlüssel
-     * @param brief Erklärung zu Einstellung
-     * @param type Der Typ der Einstellung
+     * @param key         der entsprechende Schlüssel
+     * @param brief       Erklärung zu Einstellung
+     * @param type        Der Typ der Einstellung
      * @param isMandatory Ist die Einstellung verpflichtend?
      *
      * @see SettingDeskriptor#create(String, String, eSetting_Type, Serializable)
      *
      * @return das alte Objekt, wenn vorhanden
      */
-    public SettingDeskriptor<String> emplace(String key, String brief, eSetting_Type type, boolean isMandatory){
+    public SettingDeskriptor<String> emplace(String key, String brief, eSetting_Type type, boolean isMandatory) {
         return settings.emplace(translator, key, brief, type, isMandatory);
     }
 
-
     /**
-     * Liefert eine Einstellung auf Basis eines mithilfe des {@link SettingsTranslator} übersetzen
-     * Keys
+     * Liefert eine Einstellung auf Basis eines mithilfe des
+     * {@link SettingsTranslator} übersetzen Keys
      *
      * @see Settings#requestValue(Translator, Object)
      *
      * @param key der Key
      * @return den entsprechenden Wert
      */
-    public String requestValue(String key){
+    public String requestValue(String key) {
         return settings.requestValue(translator, key);
     }
 
-
-
     /**
-     * Expandiert alle Einstellungen in dieser Zeichenfolge. Es wird gesucht nach: $<Settings> und dementsprechend
-     * expandiert. So wird zum Beispiel $Version zu {@value Definitions#JAKE_VERSION}
+     * Expandiert alle Einstellungen in dieser Zeichenfolge. Es wird gesucht nach:
+     * $<Settings> und dementsprechend expandiert. So wird zum Beispiel $Version zu
+     * {@value Definitions#JAKE_VERSION}
      *
      * @note Bitte erweitern: CoreSettingsTest>_test_expand_basics
      *
      * @param str der zu expandierende String
      * @return der expandierte string
      */
-    public static String expand(String str){
+    public static String expand(String str) {
         init();
-        for(Map.Entry<String, SettingDeskriptor<String>> s : settings){
-            str = str.replaceAll("\\$"+ Pattern.quote(s.getKey()), s.getValue().getValue());
+        for (Map.Entry<String, SettingDeskriptor<String>> s : settings) {
+            str = str.replaceAll("\\$" + Pattern.quote(s.getKey()), s.getValue().getValue());
         }
         return str;
     }
@@ -149,13 +148,14 @@ public class CoreSettings {
     /**
      *
      */
-    public static String get(String key){
-        init(); return settings.getValue(key);
+    public static String get(String key) {
+        init();
+        return settings.getValue(key);
     }
 
-    public static boolean set(String key,String val){
-        init(); return settings.set(key, val);
+    public static boolean set(String key, String val) {
+        init();
+        return settings.set(key, val);
     }
-
 
 }
