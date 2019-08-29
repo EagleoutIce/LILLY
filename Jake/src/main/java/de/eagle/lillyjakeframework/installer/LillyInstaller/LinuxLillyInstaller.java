9e1dd5ccfd6d3ad27c41a16585cf992f6610359b
EagleoutIce <maeusepiep99@web.de>
2019-08-29 15:04:27 +0200

    Some smaller fixes for the installer
    Signed-off-by: EagleoutIce <maeusepiep99@web.de>

--- Jake/src/main/java/de/eagle/lillyjakeframework/installer/LillyInstaller/LinuxLillyInstaller.java
+++ Jake/src/main/java/de/eagle/lillyjakeframework/installer/LillyInstaller/LinuxLillyInstaller.java
@@ -22,12 +22,15 @@ import de.eagle.util.helper.CommandLine;
 import de.eagle.util.helper.Executer;
 import de.eagle.util.io.JakeWriter;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static de.eagle.util.io.JakeLogger.*;
 
@@ -37,7 +40,9 @@ public class LinuxLillyInstaller extends AutoInstaller {
         steps = new FunctionCollector<>(Map.ofEntries(
                 Map.entry("check_pdftex", new FunctionDeskriptor<>("fkt_check_pdflatex","Teste, ob 'pdflatex' vorhanden ist", LinuxLillyInstaller::fkt_check_pdflatex)),
                 Map.entry("create_install_path", new FunctionDeskriptor<>("fkt_create_install_path","Erstellt das Installationsziel", LinuxLillyInstaller::fkt_create_install_path)),
-                Map.entry("install_lilly", new FunctionDeskriptor<>("fkt_install_lilly","Installiere Lilly", LinuxLillyInstaller::fkt_install_lilly))
+                Map.entry("install_lilly", new FunctionDeskriptor<>("fkt_install_lilly","Installiere Lilly", LinuxLillyInstaller::fkt_install_lilly)),
+                Map.entry("inform_tex", new FunctionDeskriptor<>("fkt_inform_tex","Informiert TeX über die Installation", LinuxLillyInstaller::fkt_inform_tex)),
+                Map.entry("search_packages", new FunctionDeskriptor<>("fkt_search_packages","Suche die notwendigen TeX-Pakete", LinuxLillyInstaller::fkt_search_packages))
         ));
         this.progress = 0;
         next = "check_pdftex";
@@ -111,12 +116,6 @@ public class LinuxLillyInstaller extends AutoInstaller {
                     f.getAbsolutePath(), g,
                     Arrays.toString(Executer.runBashCommand("ln -sf " + CoreSettings.requestValue("S_LILLY_PATH") + " " + g).lines().toArray(String[]::new))
             );
-
-            // TODO: information über angabe des Pfades
-            JakeWriter.out.format("Informiere TEX über (%s): %s%n",i,
-                    Arrays.toString(Executer.runBashCommand("texhash " + i).lines().toArray(String[]::new)));
-            return true;
-            // TODO: As last step: search for packages
         }
         return false;
     }
@@ -151,7 +150,7 @@ public class LinuxLillyInstaller extends AutoInstaller {
             JakeWriter.out.println("Jake kann Lilly mit Version 2.0.0 auf 2 verschiedene Arten installieren:");
             JakeWriter.out.println("   1) Verlinkung der gefunden Lilly-Instanz (" + target.getPath() + ")");
             JakeWriter.out.println("   2) Installation der in Jake enthaltenen Variante von Lilly");
-            switch (CommandLine.get_answer("Gebe deine Auswahl ein [1/2/(c)ancel]> ", new String[]{"1","2","c"})) {
+            switch (CommandLine.get_answer("Gebe deine Auswahl ein [1/2/(c)ancel]> ", new String[]{"1","2","C"})) {
                 case "1":
                     link_existing(target); break;
                 case "2":
@@ -162,7 +161,7 @@ public class LinuxLillyInstaller extends AutoInstaller {
                     }
                     break;
                 default:
-                case"c":
+                case "C":
                     JakeWriter.out.println("Abbruch!");
                     return END;
             }
@@ -175,15 +174,40 @@ public class LinuxLillyInstaller extends AutoInstaller {
             }
         }
 
+        return new String[] {"inform_tex", "Informiere TeX über die Installation"};
+    }
+
+    public static String[] fkt_inform_tex(String s) {
+        JakeWriter.out.format("Informiere TEX über (%s): %s%n", CoreSettings.requestValue("S_INSTALL_PATH"),
+                Arrays.toString(Executer.runBashCommand("texhash " + CoreSettings.requestValue("S_INSTALL_PATH")).lines().toArray(String[]::new)));
         JakeWriter.out.println("Lilly installiert");
+        return new String[] {"search_packages", "Suche die von Lilly verwendeten Pakete"};
+        // TODO: As last step: search for packages
+    }
+
+    public static String[] fkt_search_packages(String s) {
+        String search_command = "for i in $(cd " +  CoreSettings.requestValue("S_INSTALL_PATH") + "/tex/latex && grep -E " + // for init and cd
+                "\"(LILLYxDemandPackage|LILLYxLoadPackage){([a-zA-Z]+[a-zA-Z0-9]*)}\" -r * " + // Grep Pattern for Demand- and LoadPackages
+                "-hos | grep -E '[a-zA-Z]+[a-zA-Z0-9]*' -os); do if " + // extract Data
+                "[ \"$i\" = \"LILLYxLoadPackage\" ]; then export a=load && continue; elif " + // Identify Load
+                "[ \"$i\" = \"LILLYxDemandPackage\" ]; then export a=demand && continue; fi; " + // Identify Demand
+                "echo \"$a § $i\"; done;"; // output
+        BufferedReader out = Executer.runBashCommand(search_command);
+        //out.lines().forEach(JakeWriter.out::println);
+        Map<Boolean, List<String[]>> m = out.lines().map(a -> a.split(" § ")).filter(a -> a.length == 2).collect(Collectors.partitioningBy(a -> a[0].equals("load")));
+        JakeWriter.out.println();
+        JakeWriter.out.println("This are the Packages necessary for Lilly:");
+        JakeWriter.out.println(ColorConstants.STY_PARAM + String.join(ColorConstants.COL_RESET+ ", " + ColorConstants.STY_PARAM, m.get(false).stream().map(a -> a[1]).collect(Collectors.toSet())) + ColorConstants.COL_RESET);
+        JakeWriter.out.println("This are the Packages optional for Lilly:");
+        JakeWriter.out.println(ColorConstants.STY_PARAM + String.join(ColorConstants.COL_RESET+ ", " + ColorConstants.STY_PARAM, m.get(true).stream().map(a -> a[1]).collect(Collectors.toSet())) + ColorConstants.COL_RESET);
         return END;
     }
 
-        /**
-         * Validiert, ob alles korrekt installiert wurde
-         *
-         * @return true, wenn ja, sonst false
-         */
+    /**
+     * Validiert, ob alles korrekt installiert wurde
+     *
+     * @return true, wenn ja, sonst false
+     */
     @Override
     public boolean validate() {
         return false;
